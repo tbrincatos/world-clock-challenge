b9dab83257411f5660ef1b52a0d0091e0e0273fd
tbrincatos <88725546+tbrincatos@users.noreply.github.com>
2022-11-23 14:26:26 +0100

    css + js
    - Updated some css
    - started js file with test info

---
+++ src/index.js
@@ -0,0 +1,22 @@
+function showNewSelect(event) {
+  if (event.target.value === "africa") {
+    alert(`Hi Africa!`);
+  } else if (event.target.value === "asia") {
+    alert(`Hi Asia!`);
+  }
+  if (event.target.value === "australia") {
+    alert(`Hi Australia!`);
+  }
+  if (event.target.value === "europe") {
+    alert(`Hi Europe!`);
+  }
+  if (event.target.value === "northAmerica") {
+    alert(`Hi North America!`);
+  }
+  if (event.target.value === "southAmerica") {
+    alert(`Hi South America!`);
+  }
+}
+
+let continentSelect = document.querySelector("#continents");
+continentSelect.addEventListener("change", showNewSelect);
